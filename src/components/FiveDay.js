a6826c7ed6800a0c02f7a4c77c5ce532f0281ade
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2018-07-05 23:12:52 -0700

    working on high/low for 5 day forecast

--- src/components/FiveDay.js
+++ src/components/FiveDay.js
@@ -5,43 +5,78 @@ class FiveDay extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentDay: new Date().getDay(),
       day_0: [],
       day_1: [],
       day_2: [],
       day_3: [],
-      day_4: [],
-      day_5: [],
-      day_6: [],
+      day_4: []
     }
   }
 
   splitListArray() {
-    var sun = []
-    var mon = []
-    var tue = []
-    var wed = []
-    var thur = []
-    var fri = []
-    var sat = []
-    this.props.weather.list.forEach(list => {
-      if ((new Date(list.dt * 1000).getDay()) === 0) {sun.push(list)}
-      if ((new Date(list.dt * 1000).getDay()) === 1) {mon.push(list)}
-      if ((new Date(list.dt * 1000).getDay()) === 2) {tue.push(list)}
-      if ((new Date(list.dt * 1000).getDay()) === 3) {wed.push(list)}
-      if ((new Date(list.dt * 1000).getDay()) === 4) {thur.push(list)}
-      if ((new Date(list.dt * 1000).getDay()) === 5) {fri.push(list)}
-      if ((new Date(list.dt * 1000).getDay()) === 6) {sat.push(list)}
-    })
-    this.setState({
-      day_0: sun,
-      day_1: mon,
-      day_2: tue,
-      day_3: wed,
-      day_4: thur,
-      day_5: fri,
-      day_6: sat
-    })
+    var zero = []
+    var one = []
+    var two = []
+    var three = []
+    var four = []
+    if(new Date().getDay() === 0 || new Date().getDay() === 1) {
+      this.props.weather.list.forEach(list => {
+        if ((new Date(list.dt * 1000).getDay()) === (new Date().getDay() + 1)) {zero.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === (new Date().getDay() + 2)) {one.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === (new Date().getDay() + 3)) {two.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === (new Date().getDay() + 4)) {three.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === (new Date().getDay() + 5)) {four.push(list)}
+        var total = 0
+        console.log(zero.reduce((a,b) => ({total: a.main.temp + b.main.temp})))
+      })
+    } else if(new Date().getDay() === 2) {
+      this.props.weather.list.forEach(list => {
+        if ((new Date(list.dt * 1000).getDay()) === 3) {zero.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 4) {one.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 5) {two.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 5) {three.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 0) {four.push(list)}
+      })
+    } else if(new Date().getDay() === 3) {
+      this.props.weather.list.forEach(list => {
+        if ((new Date(list.dt * 1000).getDay()) === 4) {zero.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 5) {one.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 6) {two.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 0) {three.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 1) {four.push(list)}
+      })
+    } else if(new Date().getDay() === 4) {
+      this.props.weather.list.forEach(list => {
+        if ((new Date(list.dt * 1000).getDay()) === 5) {zero.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 6) {one.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 0) {two.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 1) {three.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 2) {four.push(list)}
+      })
+    } else if(new Date().getDay() === 5) {
+      this.props.weather.list.forEach(list => {
+        if ((new Date(list.dt * 1000).getDay()) === 6) {zero.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 0) {one.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 1) {two.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 2) {three.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 3) {four.push(list)}
+      })
+    } else if(new Date().getDay() === 6) {
+      this.props.weather.list.forEach(list => {
+        if ((new Date(list.dt * 1000).getDay()) === 0) {zero.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 1) {one.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 2) {two.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 3) {three.push(list)}
+        if ((new Date(list.dt * 1000).getDay()) === 4) {four.push(list)}
+      })
+    }
+    this.setState({ day_0: zero, day_1: one, day_2: two, day_3: three, day_4: four })
+  }
+
+  findMinTemp(array) {
+    array.reduce((a,b) => {
+      return a.main.temp_min < b.main.temp_min ? 'a' : 'b'
+    }, 1000)
   }
 
   componentDidMount() {
@@ -53,37 +88,30 @@ class FiveDay extends React.Component {
   render() {
 
     const currentDay = this.state.currentDay
+    const day_0 = this.state.day_0
 
     return(
       <div>
-        <h1>Five Day Forecast</h1>
-        {this.state.currentDay === 0 && this.state.day_3[0]
-          ? <div><h1>{this.state.day_0[0].main.temp}</h1></div>
-          : null
-        }
-        {this.state.currentDay === 1 && this.state.day_3[0]
-          ? <div><h1>{this.state.day_1[0].main.temp}</h1></div>
-          : null
-        }
-        {this.state.currentDay === 2 && this.state.day_3[0]
-          ? <div><h1>{this.state.day_2[0].main.temp}</h1></div>
-          : null
-        }
-        {this.state.currentDay === 3 && this.state.day_3[0]
-          ? <div><h1>{this.state.day_3[0].main.temp}</h1></div>
-          : null
-        }
-        {this.state.currentDay === 4 && this.state.day_3[0]
-          ? <div><h1>{this.state.day_4[0].main.temp}</h1></div>
-          : null
-        }
-        {this.state.currentDay === 5 && this.state.day_3[0]
-          ? <div><h1>{this.state.day_5[0].main.temp}</h1></div>
-          : null
-        }
-        {this.state.currentDay === 6 && this.state.day_3[0]
-          ? <div><h1>{this.state.day_6[0].main.temp}</h1></div>
-          : null
+        {this.state.day_0.length > 0
+          ? <div>
+              <div className='fiveDay-card'>
+                <h1>{Math.round((day_0.reduce((acc, obj) => { return acc + obj.main.temp; }, 0) / day_0.length) * 9/5 - 459.67) + '˚F'}</h1>
+                <h1>Low: {this.findMinTemp(day_0).bind(this)}</h1>
+              </div>
+              <div className='fiveDay-card'>
+                <h1>{Math.round((this.state.day_1.reduce((acc, obj) => { return acc + obj.main.temp; }, 0) / this.state.day_1.length) * 9/5 - 459.67) + '˚F'}</h1>
+              </div>
+              <div className='fiveDay-card'>
+                <h1>{Math.round((this.state.day_2.reduce((acc, obj) => { return acc + obj.main.temp; }, 0) / this.state.day_2.length) * 9/5 - 459.67) + '˚F'}</h1>
+              </div>
+              <div className='fiveDay-card'>
+                <h1>{Math.round((this.state.day_3.reduce((acc, obj) => { return acc + obj.main.temp; }, 0) / this.state.day_3.length) * 9/5 - 459.67) + '˚F'}</h1>
+              </div>
+              <div className='fiveDay-card'>
+                <h1>{Math.round((this.state.day_4.reduce((acc, obj) => { return acc + obj.main.temp; }, 0) / this.state.day_4.length) * 9/5 - 459.67) + '˚F'}</h1>
+              </div>
+            </div>
+          : <h1>Retrieving Your Forecast</h1>
         }
       </div>
     )
