817d08a090ea20ed2de340ca26fc59588eb6d9ae
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2018-07-06 01:36:41 -0700

    changed footer slightly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ class Footer extends React.Component {
     return (
       <div className='footer'>
         <h4>AW Weather App</h4>
+        <h4>Created by Matthew Westenhaver</h4><br />
         <h4>Thank you to openweathermap.org for the API</h4>
-        <h4>Created by Matthew Westenhaver</h4>
       </div>
     )
   }
